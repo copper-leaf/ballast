cd89c4a30f1664ed3140f943d91575001fc58dad
Casey Brooks <cjbrooks12@gmail.com>
2022-03-18 15:05:53 -0500

    [minor] Renames `ballast-crashlytics` to `ballast-firebase-crashlytics`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "ballast"
 include(":ballast-core")
 include(":ballast-repository")
 
-include(":ballast-crashlytics")
+include(":ballast-firebase-crashlytics")
 include(":ballast-firebase-analytics")
 
 include(":ballast-debugger")
