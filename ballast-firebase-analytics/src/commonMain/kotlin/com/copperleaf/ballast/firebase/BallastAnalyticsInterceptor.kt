a7789c59ab3298615f78134163020c3c0b0a3195
Casey Brooks <cjbrooks12@gmail.com>
2023-01-05 16:09:34 -0600

    Make Firebase Analytics more flexible

---
+++ ballast-firebase-analytics/src/commonMain/kotlin/com/copperleaf/ballast/firebase/BallastAnalyticsInterceptor.kt
@@ -0,0 +1,32 @@
+package com.copperleaf.ballast.firebase
+
+import com.copperleaf.ballast.BallastInterceptor
+import com.copperleaf.ballast.BallastLogger
+import com.copperleaf.ballast.BallastNotification
+
+public class BallastAnalyticsInterceptor<Inputs : Any, Events : Any, State : Any>(
+    private val tracker: AnalyticsTracker,
+    private val shouldTrackInput: (Inputs) -> Boolean,
+) : BallastInterceptor<Inputs, Events, State> {
+
+    private object Keys {
+        const val ViewModelName = "ViewModelName"
+        const val InputType = "InputType"
+        const val InputValue = "InputValue"
+    }
+
+    override suspend fun onNotify(logger: BallastLogger, notification: BallastNotification<Inputs, Events, State>) {
+        if (notification is BallastNotification.InputAccepted) {
+            if (shouldTrackInput(notification.input)) {
+                tracker.trackAnalyticsEvent(
+                    "action",
+                    mapOf(
+                        Keys.ViewModelName to notification.vm.name,
+                        Keys.InputType to "${notification.vm.name}.${notification.input::class.simpleName}",
+                        Keys.InputValue to "${notification.vm.name}.${notification.input}",
+                    )
+                )
+            }
+        }
+    }
+}
