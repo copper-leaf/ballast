ac39abf54c9ba54ac97e2eb6f202a3b544acb969
Casey Brooks <cjbrooks12@gmail.com>
2023-04-19 10:47:39 -0400

    Allow slight customization of analytics data

--- ballast-analytics/src/commonMain/kotlin/com/copperleaf/ballast/analytics/AnalyticsTracker.kt
+++ ballast-analytics/src/commonMain/kotlin/com/copperleaf/ballast/analytics/AnalyticsTracker.kt
@@ -2,6 +2,9 @@ package com.copperleaf.ballast.analytics
 
 public interface AnalyticsTracker {
 
+    /**
+     * Record an event with an analytics SDK.
+     */
     public fun trackAnalyticsEvent(eventId: String, eventParameters: Map<String, String>)
 
 }
