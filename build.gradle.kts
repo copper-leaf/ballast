9a916e750fbd1474a089d420948730a7c8856d94
Casey Brooks <cjbrooks12@gmail.com>
2025-12-27 19:15:34 -0600

    Updates API files

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,14 @@ apiValidation {
     ignoredProjects.addAll(
         listOf(
 //            "docs",
-//            "android",
-//            "counter",
-//            "desktop",
-//            "navigationWithCustomRoutes",
-//            "navigationWithEnumRoutes",
-//            "schedules",
-//            "web",
-//            "ballast-idea-plugin",
+            "android",
+            "counter",
+            "desktop",
+            "navigationWithCustomRoutes",
+            "navigationWithEnumRoutes",
+            "schedules",
+            "web",
+            "ballast-idea-plugin",
         )
     )
 }
